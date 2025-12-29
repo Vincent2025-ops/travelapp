31de470dc409d5cad25c234d254ef1140b956b33
unknown <j25329021@gmail.com>
2025-12-29 17:06:39 +0800

    Deploy full project to GitHub

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import TravelApp from './travelapp_v9test.jsx' // 👈 這裡必須對應您的檔名
+import TravelApp from './travelapp_v9.jsx' // 👈 這裡必須對應您的檔名
 import './index.css' // 👈 關鍵！一定要有這一行，不然畫面會變全白或只有純文字
 
 ReactDOM.createRoot(document.getElementById('root')).render(
